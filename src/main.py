3f0731b2748555cb5ff033b3797d79b6ed3534f5
max <DubrovinMax1256@gmail.com>
2025-09-30 20:13:40 +0300

    add docker compose file

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from core.config import settings
 import uvicorn
 
 
@@ -6,5 +7,8 @@ app = FastAPI()
 
 if __name__=="__main__":
     uvicorn.run(
-        "main:app"
+        "main:app",
+        port=settings.srv.port,
+        host=settings.srv.host,
+        reload=True,
     )
